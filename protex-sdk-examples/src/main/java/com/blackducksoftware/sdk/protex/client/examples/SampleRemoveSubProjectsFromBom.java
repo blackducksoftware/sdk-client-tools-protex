01f18e3bca43050e8031d66f11a2b72d3964c0fd
Abinaya Pitchai <apitchai@blackducksoftware.com>
2016-10-27 10:20:07 -0400

    PROTEX-21965:Wrap the sdk method calls with try catch

--- protex-sdk-examples/src/main/java/com/blackducksoftware/sdk/protex/client/examples/SampleRemoveSubProjectsFromBom.java
+++ protex-sdk-examples/src/main/java/com/blackducksoftware/sdk/protex/client/examples/SampleRemoveSubProjectsFromBom.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.blackducksoftware.sdk.fault.SdkFault;
 import com.blackducksoftware.sdk.protex.client.util.ProtexServerProxy;
 import com.blackducksoftware.sdk.protex.common.BomRefreshMode;
 import com.blackducksoftware.sdk.protex.common.ComponentType;
@@ -84,48 +85,89 @@ public class SampleRemoveSubProjectsFromBom extends BDProtexSample {
         ProtexServerProxy myProtexServer = null;
 
         try {
-            myProtexServer = new ProtexServerProxy(serverUri, username, password, connectionTimeout);
-
-            projectApi = myProtexServer.getProjectApi();
-            identificationApi = myProtexServer.getIdentificationApi();
-            bomApi = myProtexServer.getBomApi();
-
             try {
-                ProjectInfoPageFilter projectPageFilter = new ProjectInfoPageFilter();
-                projectPageFilter.setFirstRowIndex(Integer.parseInt(firstIndex));
-                projectPageFilter.setLastRowIndex(Integer.parseInt(lastIndex));
-                projectPageFilter.setSortedColumn(ProjectInfoColumn.PROJECT_ID);
-                projectPageFilter.setSortType(SortType.ALPHABETICAL_CASE_INSENSITIVE);
-                projectPageFilter.setSortAscending(true);
-                List<ProjectInfo> projectsInfo = projectApi.suggestProjects("", projectPageFilter);
+                myProtexServer = new ProtexServerProxy(serverUri, username, password, connectionTimeout);
 
-                for (ProjectInfo pInfo : projectsInfo) {
-                    String projectId = pInfo.getProjectId();
+                projectApi = myProtexServer.getProjectApi();
+                identificationApi = myProtexServer.getIdentificationApi();
+                bomApi = myProtexServer.getBomApi();
 
-                    // Getting all the bom components of this project and filtering out the just the subprojects
-                    List<BomComponent> allBomComponents = bomApi.getBomComponents(projectId);
-                    List<BomComponent> subProjects = new LinkedList<BomComponent>();
-                    for (BomComponent bComponent : allBomComponents) {
-                        if (bComponent.getComponentType().equals(ComponentType.PROJECT)
-                                && !bComponent.getComponentKey().getComponentId().equals(projectId)) {
-                            subProjects.add(bComponent);
-                        }
+            } catch (RuntimeException e) {
+                System.err.println("Connection to server '" + serverUri + "' failed: " + e.getMessage());
+                throw e;
+            }
+
+            // Get the list of projects whose subprojects needs to be removed from BOM
+            ProjectInfoPageFilter projectPageFilter = new ProjectInfoPageFilter();
+            projectPageFilter.setFirstRowIndex(Integer.parseInt(firstIndex));
+            projectPageFilter.setLastRowIndex(Integer.parseInt(lastIndex));
+            projectPageFilter.setSortedColumn(ProjectInfoColumn.PROJECT_ID);
+            projectPageFilter.setSortType(SortType.ALPHABETICAL_CASE_INSENSITIVE);
+            projectPageFilter.setSortAscending(true);
+
+            List<ProjectInfo> projectsInfo = new LinkedList<ProjectInfo>();
+            try {
+                projectsInfo = projectApi.suggestProjects("", projectPageFilter);
+            } catch (SdkFault e) {
+                System.err.println("suggestProjects() failed" + e.getMessage());
+                throw new RuntimeException(e);
+            }
+
+            for (ProjectInfo pInfo : projectsInfo) {
+                String projectId = pInfo.getProjectId();
+
+                // Getting all the bom components of this project and filtering out the just the subprojects
+                List<BomComponent> allBomComponents = new LinkedList<BomComponent>();
+                try {
+                    allBomComponents = bomApi.getBomComponents(projectId);
+                } catch (SdkFault e) {
+                    System.err.println("getBomComponents() failed" + e.getMessage());
+                    throw new RuntimeException(e);
+                }
+
+                List<BomComponent> subProjects = new LinkedList<BomComponent>();
+                for (BomComponent bComponent : allBomComponents) {
+                    if (bComponent.getComponentType().equals(ComponentType.PROJECT)
+                            && !bComponent.getComponentKey().getComponentId().equals(projectId)) {
+                        subProjects.add(bComponent);
+                    }
+                }
+
+                List<IdentificationType> idTypes = Arrays.asList(IdentificationType.CODE_MATCH, IdentificationType.DECLARATION,
+                        IdentificationType.DEPENDENCY,
+                        IdentificationType.STRING_SEARCH);
+                List<IdentificationMode> idModes = Arrays.asList(IdentificationMode.MANUAL, IdentificationMode.RAPID_ID);
+
+                // Finding the identifications of the subprojects and resetting the identifications.
+                // If no identifications are present, removing the subproject from bom component.
+                for (BomComponent bComponent : subProjects) {
+                    List<CodeTreeNode> idNodes = null;
+                    try {
+                        idNodes = bomApi.getFilesIdentifiedTo(projectId, bComponent.getComponentKey(), idTypes, idModes);
+                    } catch (SdkFault e) {
+                        System.err.println("getFilesIdentifiedTo() failed" + e.getMessage());
+                        throw new RuntimeException(e);
                     }
 
-                    List<IdentificationType> idTypes = Arrays.asList(IdentificationType.CODE_MATCH, IdentificationType.DECLARATION,
-                            IdentificationType.DEPENDENCY,
-                            IdentificationType.STRING_SEARCH);
-                    List<IdentificationMode> idModes = Arrays.asList(IdentificationMode.MANUAL, IdentificationMode.RAPID_ID);
-                    // Finding the identifications of the subprojects and resetting the identifications.
-                    // If no identifications are present, removing the subproject from bom component.
-                    for (BomComponent bComponent : subProjects) {
-                        List<CodeTreeNode> idNodes = bomApi.getFilesIdentifiedTo(projectId, bComponent.getComponentKey(), idTypes, idModes);
-                        if (idNodes == null || idNodes.isEmpty()) {
+                    if (idNodes == null || idNodes.isEmpty()) {
+                        try {
                             bomApi.removeBomComponent(projectId, bComponent.getComponentKey(), true, BomRefreshMode.ASYNCHRONOUS);
-                        } else {
-                            List<CodeTreeIdentificationInfo> idsInfo = identificationApi.getEffectiveIdentifications(projectId, idNodes);
-                            for (CodeTreeIdentificationInfo idInfo : idsInfo) {
-                                for (Identification identification : idInfo.getIdentifications()) {
+                        } catch (SdkFault e) {
+                            System.err.println("removeBomComponent() failed" + e.getMessage());
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        List<CodeTreeIdentificationInfo> idsInfo = new LinkedList<CodeTreeIdentificationInfo>();
+                        try {
+                            idsInfo = identificationApi.getEffectiveIdentifications(projectId, idNodes);
+                        } catch (SdkFault e) {
+                            System.err.println("getEffectiveIdentifications() failed" + e.getMessage());
+                            throw new RuntimeException(e);
+                        }
+
+                        for (CodeTreeIdentificationInfo idInfo : idsInfo) {
+                            for (Identification identification : idInfo.getIdentifications()) {
+                                try {
                                     if (identification instanceof CodeMatchIdentification) {
                                         CodeMatchIdentification codeMatchIdentification = (CodeMatchIdentification) identification;
                                         identificationApi.removeCodeMatchIdentification(projectId, codeMatchIdentification, BomRefreshMode.ASYNCHRONOUS);
@@ -134,24 +176,33 @@ public class SampleRemoveSubProjectsFromBom extends BDProtexSample {
                                         identificationApi.removeDeclaredIdentification(projectId, declaredIdentification, BomRefreshMode.ASYNCHRONOUS);
                                     } else if (identification instanceof StringSearchIdentification) {
                                         StringSearchIdentification stringSearchIdentification = (StringSearchIdentification) identification;
-                                        identificationApi.removeStringSearchIdentification(projectId, stringSearchIdentification, BomRefreshMode.ASYNCHRONOUS);
+                                        identificationApi.removeStringSearchIdentification(projectId, stringSearchIdentification,
+                                                BomRefreshMode.ASYNCHRONOUS);
                                     } else if (identification instanceof DependencyIdentification) {
                                         DependencyIdentification dependencyIdentification = (DependencyIdentification) identification;
                                         identificationApi.removeDependencyIdentification(projectId, dependencyIdentification, BomRefreshMode.ASYNCHRONOUS);
                                     }
+                                } catch (SdkFault e) {
+                                    System.err.println("remove Identification failed" + e.getMessage());
+                                    throw new RuntimeException(e);
                                 }
                             }
                         }
                     }
                 }
-
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
+                System.out.println("Remove subproject identification for \"" + projectId + "\" finished.");
             }
 
-        } catch (RuntimeException e) {
-            System.err.println("Connection to server '" + serverUri + "' failed: " + e.getMessage());
-            throw e;
+        } catch (Exception e) {
+            System.err.println("Sample RemoveSubProjects failed");
+            e.printStackTrace(System.err);
+            System.exit(-1);
+        } finally {
+            // This is optional - it causes the proxy to overwrite the stored password with null characters, increasing
+            // security
+            if (myProtexServer != null) {
+                myProtexServer.close();
+            }
         }
     }
 }
